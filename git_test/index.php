c4e941e1db307a0100259f4fdc8f3546fbaf3e70
kobe-architects <kobe.architects@gmail.com>
2023-03-30 00:52:02 +0900

    tomoaki test

---
+++ git_test/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<title>git hub test</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<link rel="stylesheet" href="css/reset.css" type="text/css">
+<link rel="stylesheet" href="css/index.css" type="text/css">
+</head>
+
+<body>
+	<h1>git hub test</h1>
+</body>
+<script src="js/index.js"></script>
+
+</html>
